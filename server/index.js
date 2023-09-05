7142cb0479a1be562c31fc6e88656226f6a6e5e8
James <jamesezhang@tencent.com>
2023-09-05 21:46:01 +0800

    docs: update docs

--- server/index.js
+++ server/index.js
@@ -1,17 +1,6 @@
 const express = require('express');
 const app = express();
 
-/* app.use(function (req, res, next) {
-    console.log(req.url);
-    if (req.url === '/hello.css') {
-        setTimeout(next, 1000)
-    } else if (req.url === '/hello.js') {
-        setTimeout(next, 2000)
-    } else {
-        next();
-    }
-}); */
-
 app.use(express.static('public'));
 
 app.listen(80, () => {
